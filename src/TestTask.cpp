b02bc912e795c97e63158ec9e2c02e659dd13919
shynur <shynur@outlook.com>
2025-03-28 14:40:08 +0800

    ;

--- src/TestTask.cpp
+++ src/TestTask.cpp
@@ -3,17 +3,15 @@
 #include <ranges>
 
 Task completes_synchronously() {
-    Logger{__PRETTY_FUNCTION__};
     co_return;
 }
 
 Task loop_synchronously(const std::size_t count) {
-    Logger{__PRETTY_FUNCTION__};
     for (auto _ : std::views::iota(0u, count))
         co_await completes_synchronously();
 }
 
 int main() {
     ManualExecutor e;
-    e.sync_wait(loop_synchronously(0));
+    e.sync_wait(loop_synchronously(1111110));
 }
